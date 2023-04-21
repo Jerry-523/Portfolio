d66a9816a5a0e247fe28e6c8a4f4d15fd2de1cb8
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2023-04-21 09:38:22 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*Comecei minha jornada como desenvolvedor enquanto ainda estava na faculdade, quando tive a oportunidade de trabalhar em projetos interessantes que me permitiram aprender e aprimorar minhas habilidades. Desde então, tenho trabalhado em projetos desafiadores e interessantes em diversas empresas, o que me ajudou a crescer profissionalmente e a expandir meu conhecimento.
+                    
+Além de minhas habilidades técnicas, sou comprometido com a entrega de projetos de alta qualidade e adoro trabalhar em equipe. Acredito que a colaboração é fundamental para o sucesso de qualquer projeto e estou sempre disposto a ouvir ideias e sugestões para melhorar o trabalho em equipe.
+                    
+Gosto de desafios e estou sempre procurando novas oportunidades para me desenvolver e aprender novas tecnologias.
+
+Fora do trabalho, gosto de passar meu tempo livre explorando novas tecnologias e ferramentas, participando de eventos da comunidade de desenvolvimento e contribuindo para projetos de código aberto.
+
+Se você está procurando um desenvolvedor Full Stack altamente motivado e apaixonado por tecnologia, estou à disposição para colaborar em projetos interessantes e desafiadores. Não hesite em entrar em contato comigo para discutir suas necessidades e ver como posso ajudar!*/
